c5a339bb0dce32061fb032a6d8395f59abfdc4e7
SwitchTia <oblasser.tatiana@gmail.com>
2026-05-20 22:31:31 +0200

    Feat: backend update

---
+++ types.ts
@@ -0,0 +1,12 @@
+export type Model = {
+  title: string;
+  category: string;
+  provider?: string;
+  releaseYear?: number;
+  modality?: string;
+  contextWindow?: string;
+  pricingTier?: string;
+  strengths?: string;
+  description?: string;
+};
+
